184fd11ee33d9433cd882778ee003f863600b3f4
“AndreuMartinez” <andreu.martinez@illusionstudio.es>
2022-09-21 13:25:29 +0200

    feat: Añadida logica para login del usuario y respuesta del backend

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,18 +1,33 @@
-// const authConfig = require('../config/auth');
-// const { Usuario } = require('../models/index');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const { Op } = require("sequelize");
-
 const UsuarioController = {};
 
-
 //Funciones del controlador de usuarios
-
 
 //Login de un usuario registrado
 UsuarioController.logUsuario = (req, res) => {
 
+    let correo = req.body.email;
+    let password = req.body.password;
+    let correoGuardado = "jhon.smith@akapp.test";
+    let passwordGuardado = "enjoyfirst";
+
+    if ( correoGuardado != correo || passwordGuardado != password) {
+
+        //Credenciales incorrectas, enviamos "401 unauthorized" mas un msg y el codigo de estatus 0 al deberse a un error.
+        res.status(401).json({ 
+            msg: "Usuario o contraseña inválidos", 
+            status: 0 
+        });
+
+    } else {
+
+        //Las credenciales son correctas
+        res.status(200).json({
+            name : "Jhon",
+            surname : "Smith",
+            status : 1
+        })
+
+    };
 
 };
 
